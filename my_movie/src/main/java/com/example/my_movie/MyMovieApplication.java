0f54596ec5a8d7dd5141adc409db166311d3353e
Geoffrey <geoffrey0151@gmail.com>
2023-01-31 13:10:52 +0100

    initialisation du MS my_movie

---
+++ my_movie/src/main/java/com/example/my_movie/MyMovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMovieApplication.class, args);
+	}
+
+}
---
+++ my_movie/src/test/java/com/example/my_movie/MyMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
