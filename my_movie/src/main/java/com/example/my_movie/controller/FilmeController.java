39be9977eefcec031a73d983adac47b905e1214f
Geoffrey <geoffrey0151@gmail.com>
2023-01-31 14:40:17 +0100

    ajout de la dépendance et annotations swagger

--- my_movie/src/main/java/com/example/my_movie/controller/FilmeController.java
+++ my_movie/src/main/java/com/example/my_movie/controller/FilmeController.java
@@ -3,24 +3,29 @@ package com.example.my_movie.controller;
 import com.example.my_movie.model.Acteur;
 import com.example.my_movie.model.Filme;
 import com.example.my_movie.services.ActeurService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @RestController
+@EnableSwagger2
 public class FilmeController {
     @Autowired
     ActeurService acteurService;
 
     @RequestMapping(value = "/acteurs/")
+    @ApiOperation(value = "Avoir tous les acteurs", response = Acteur.class, tags = "getActeurs")
     public List<Acteur> getActeurs() {
         return acteurService.getActeurs();
     }
 
     @RequestMapping(value = "/acteurs/nom/{nom}")
+    @ApiOperation(value = "Avoir un acteur", response = Acteur.class, tags = "getActeur")
     public Acteur getActeur(@PathVariable(value = "nom") String nom) {
         List<Acteur> acteurs = acteurService.getActeurs();
         for (Acteur acteur : acteurs) {
@@ -35,11 +40,13 @@ public class FilmeController {
     // ------- filmes -------- //
 
     @RequestMapping(value = "/filmes/")
+    @ApiOperation(value = "Avoir tous les filmes", response = Acteur.class, tags = "getFilmes")
     public List<Filme> getFilmes() {
         return acteurService.getFilmes();
     }
 
     @RequestMapping(value = "/filmes/titre/{titre}")
+    @ApiOperation(value = "Avoir un filme", response = Acteur.class, tags = "getFilme")
     public Filme getFilme(@PathVariable(value = "titre") String titre) {
         List<Filme> filmes = acteurService.getFilmes();
         for (Filme filme : filmes) {
