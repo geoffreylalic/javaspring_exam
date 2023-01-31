52c3a03fb1a51a77352bbcac30d294a2413d838e
Geoffrey <geoffrey0151@gmail.com>
2023-01-31 13:05:03 +0100

    initialisation de my_dashboard eureka server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # javaspring_exam
 Exam Java Spring M2 INTENSE
+
+port server eureka: 3001
--- my_dashboard/src/main/java/com/example/my_dashboard/MyDashboardApplication.java
+++ my_dashboard/src/main/java/com/example/my_dashboard/MyDashboardApplication.java
@@ -2,8 +2,12 @@ package com.example.my_dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableAdminServer
 public class MyDashboardApplication {
 
 	public static void main(String[] args) {
